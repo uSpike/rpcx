05b42afd77de63751278fd145463b572e293eaa6
Declan McGraw <dmcgraw@xes-inc.com>
2025-10-31 15:39:35 -0500

    tests: cleanup anyio memory object stream warnings

--- tests/test_server.py
+++ tests/test_server.py
@@ -33,43 +33,43 @@ async def test_bad_message():
 
     bad_msg = BadMessage(0)
 
-    stream = StapledObjectStream(*anyio.create_memory_object_stream[bytes](math.inf))
-    server = RPCServer(stream, RPCManager())
-    with pytest.raises(ValueError, match="Unknown message type: 100"):
-        with collapse_excgroups():
-            async with anyio.create_task_group() as tg:
-                tg.start_soon(server.serve, True)
-                await stream.send(message_to_bytes(bad_msg))
+    async with StapledObjectStream(*anyio.create_memory_object_stream[bytes](math.inf)) as stream:
+        server = RPCServer(stream, RPCManager())
+        with pytest.raises(ValueError, match="Unknown message type: 100"):
+            with collapse_excgroups():
+                async with anyio.create_task_group() as tg:
+                    tg.start_soon(server.serve, True)
+                    await stream.send(message_to_bytes(bad_msg))
 
 
 async def test_internal_error(mocker):
-    stream = StapledObjectStream(*anyio.create_memory_object_stream[bytes](math.inf))
-    server = RPCServer(stream, RPCManager())
+    async with StapledObjectStream(*anyio.create_memory_object_stream[bytes](math.inf)) as stream:
+        server = RPCServer(stream, RPCManager())
 
-    mocker.patch.object(server, "handle_request", side_effect=Exception("boom!"))
-    msg = Request(id=1, method="foo", args=(), kwargs={})
+        mocker.patch.object(server, "handle_request", side_effect=Exception("boom!"))
+        msg = Request(id=1, method="foo", args=(), kwargs={})
 
-    with pytest.raises(Exception, match="boom!"):
-        with collapse_excgroups():
-            async with anyio.create_task_group() as tg:
-                tg.start_soon(server.serve, True)
-                await stream.send(message_to_bytes(msg))
+        with pytest.raises(Exception, match="boom!"):
+            with collapse_excgroups():
+                async with anyio.create_task_group() as tg:
+                    tg.start_soon(server.serve, True)
+                    await stream.send(message_to_bytes(msg))
 
 
 async def test_invalid_id(caplog):
-    stream = StapledObjectStream(*anyio.create_memory_object_stream[bytes](math.inf))
-    server = RPCServer(stream, RPCManager())
+    async with StapledObjectStream(*anyio.create_memory_object_stream[bytes](math.inf)) as stream:
+        server = RPCServer(stream, RPCManager())
 
-    msg = RequestCancel(id=1)
+        msg = RequestCancel(id=1)
 
-    with caplog.at_level(logging.WARNING):
-        async with anyio.create_task_group() as tg:
-            tg.start_soon(server.serve, True)
-            await stream.send(message_to_bytes(msg))
-            await anyio.wait_all_tasks_blocked()
-            tg.cancel_scope.cancel()
+        with caplog.at_level(logging.WARNING):
+            async with anyio.create_task_group() as tg:
+                tg.start_soon(server.serve, True)
+                await stream.send(message_to_bytes(msg))
+                await anyio.wait_all_tasks_blocked()
+                tg.cancel_scope.cancel()
 
-    assert "Requested non-existing task" in caplog.text
+        assert "Requested non-existing task" in caplog.text
 
 
 async def test_unhandled_message(caplog):
@@ -79,21 +79,21 @@ async def test_unhandled_message(caplog):
     rpc = RPCManager()
     rpc.register("wait", wait)
 
-    stream = StapledObjectStream(*anyio.create_memory_object_stream[bytes](math.inf))
-    server = RPCServer(stream, rpc)
+    async with StapledObjectStream(*anyio.create_memory_object_stream[bytes](math.inf)) as stream:
+        server = RPCServer(stream, rpc)
 
-    # start a task that waits forever
-    msg1 = Request(id=0, method="wait", args=(), kwargs={})
-    # send a response which doesn't make sense for the server to receive
-    msg2 = Response(id=0, status=ResponseStatus.OK, value=None)
+        # start a task that waits forever
+        msg1 = Request(id=0, method="wait", args=(), kwargs={})
+        # send a response which doesn't make sense for the server to receive
+        msg2 = Response(id=0, status=ResponseStatus.OK, value=None)
 
-    with caplog.at_level(logging.WARNING):
-        async with anyio.create_task_group() as tg:
-            tg.start_soon(server.serve, True)
-            await stream.send(message_to_bytes(msg1))
-            await anyio.wait_all_tasks_blocked()
-            await stream.send(message_to_bytes(msg2))
-            await anyio.wait_all_tasks_blocked()
-            tg.cancel_scope.cancel()
+        with caplog.at_level(logging.WARNING):
+            async with anyio.create_task_group() as tg:
+                tg.start_soon(server.serve, True)
+                await stream.send(message_to_bytes(msg1))
+                await anyio.wait_all_tasks_blocked()
+                await stream.send(message_to_bytes(msg2))
+                await anyio.wait_all_tasks_blocked()
+                tg.cancel_scope.cancel()
 
-    assert "Received unhandled message" in caplog.text
+        assert "Received unhandled message" in caplog.text
