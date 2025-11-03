898d8c5ca551e02a284e6cf723b1d5bcd404f77e
Vincent Fazio <vfazio@xes-inc.com>
2025-11-03 11:26:03 -0600

    tests: websockets: fix ConnectionClosed construction
    Upstream 62eb267c3403 changed the ConnectionClosed constructor to take a
    `frames.Close` object away from separate code/reason arguments. This was
    included in the 10.0 websockets release.
    
    Since mypy type checks are not run on tests, this was missed as part of
    the repository's initial commit.
    
    Fixes: 7a35fb873e4d ("Initial commit")
    Signed-off-by: Vincent Fazio <vfazio@xes-inc.com>

--- tests/test_websockets.py
+++ tests/test_websockets.py
@@ -3,6 +3,7 @@ from unittest.mock import AsyncMock
 import anyio
 import pytest
 from websockets.exceptions import ConnectionClosed
+from websockets.frames import Close
 
 from rpcx.websockets import WebSocketsStream
 
@@ -18,10 +19,10 @@ async def test_websockets_stream():
     await stream.send(b"foo")
     stream.websocket.send.assert_called_once()
 
-    stream.websocket.recv.side_effect = ConnectionClosed(1000, "foo")
+    stream.websocket.recv.side_effect = ConnectionClosed(Close(1000, "foo"), None)
     with pytest.raises(anyio.EndOfStream):
         await stream.receive()
 
-    stream.websocket.send.side_effect = ConnectionClosed(1000, "foo")
+    stream.websocket.send.side_effect = ConnectionClosed(Close(1000, "foo"), None)
     with pytest.raises(anyio.EndOfStream):
         await stream.send(b"foo")
